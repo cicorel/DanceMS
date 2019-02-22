34519c87d1e26a41fe534b3d972d8e5b8fb84214
Radu Alexandru <radualexandrucosmin@gmail.com>
2019-02-22 12:57:03 +0200

    Implemented Caching Tokens

--- DanceMS/DanceMS.Android/Services/DroidLoginProvider.cs
+++ DanceMS/DanceMS.Android/Services/DroidLoginProvider.cs
@@ -3,22 +3,95 @@ using Android.Content;
 using Microsoft.WindowsAzure.MobileServices;
 using DanceMS.Abstractions;
 using DanceMS.Droid.Services;
+using Xamarin.Auth;
+using System;
+using System.Text;
+using Newtonsoft.Json.Linq;
 
 [assembly: Xamarin.Forms.Dependency(typeof(DroidLoginProvider))]
 namespace DanceMS.Droid.Services
 {
     public class DroidLoginProvider : ILoginProvider
     {
-        Context context;
+        public Context Context { get; private set; }
+        public AccountStore AccountStore { get; private set; }
 
         public void Init(Context context)
         {
-            this.context = context;
+            this.Context = context;
+            AccountStore = AccountStore.Create(context);
         }
 
+        /// <summary>
+        ///  The first piece is to check to see if there is an existing token in the KeyStore. 
+        ///  If there is, we check the expiry time and then set up the Azure Mobile Apps client with the username and token from the KeyStore.
+        ///  If there isn't, we do the normal authentication process. 
+        ///  If the authentication process is successful, we reach the second piece, which is to store the token within the KeyStore.
+        ///  If there is an existing entry, it will be overwritten. 
+        ///  Finally, there is a method called IsTokenExpired() whose only job is to check to see if a token is expired or not. 
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="provider"></param>
+        /// <returns></returns>
         public async Task LoginAsync(MobileServiceClient client, MobileServiceAuthenticationProvider provider)
         {
-            await client.LoginAsync(context, provider, Constants.Constants.UrlScheme);
+
+            var accounts = AccountStore.FindAccountsForService("dancems");
+            if (accounts != null)
+            {
+                foreach(var userAccount in accounts)
+                {
+                    string token;
+
+                    if(userAccount.Properties.TryGetValue("token", out token))
+                    {
+                        if(!IsTokenExpired(token))
+                        {
+                            client.CurrentUser = new MobileServiceUser(userAccount.Username);
+                            client.CurrentUser.MobileServiceAuthenticationToken = token;
+                            return;
+                        }
+                    }
+                }
+            }
+
+            await client.LoginAsync(Context, provider, Constants.Constants.UrlScheme);
+
+            var account = new Account(client.CurrentUser.UserId);
+            account.Properties.Add("token", client.CurrentUser.MobileServiceAuthenticationToken);
+            AccountStore.Save(account, "dancems");
+        }
+
+        private bool IsTokenExpired(string token)
+        {
+            //Get just the JWT part of the token
+            var jwt = token.Split(new char[] { '.', '/' })[1];
+
+            //Unde the URL encoding
+            jwt = jwt.Replace('-', '+').Replace('_', '/');
+            switch (jwt.Length % 4)
+            {
+                case 0: break;
+                case 2: jwt += "=="; break;
+                case 3: jwt += "="; break;
+                default:
+                    throw new ArgumentException("The token is not a valid Base64 string.");
+            }
+
+            // Convert to a JSON String
+            var bytes = Convert.FromBase64String(jwt);
+            string jsonString = UTF8Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+
+            // Parse as JSON object and get the exp field value,
+            // which is the expiration date as a JavaScript primative date.
+            JObject jsonObj = JObject.Parse(jsonString);
+            var exp = Convert.ToDouble(jsonObj["exp"].ToString());
+
+            // Calculate the expiration by adding the exp value (in seconds) to the
+            // base date of 1/1/1970.
+            DateTime minTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            var expire = minTime.AddSeconds(exp);
+            return (expire < DateTime.UtcNow);
         }
     }
 }
--- DanceMS/DanceMS.UWP/Services/UWPLoginProvider.cs
+++ DanceMS/DanceMS.UWP/Services/UWPLoginProvider.cs
@@ -3,20 +3,55 @@ using Microsoft.WindowsAzure.MobileServices;
 using DanceMS.Abstractions;
 using DanceMS.UWP.Services;
 using System;
+using Windows.Security.Credentials;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
 
 [assembly: Xamarin.Forms.Dependency(typeof(UWPLoginProvider))]
 namespace DanceMS.UWP.Services
 {
     public class UWPLoginProvider : ILoginProvider
     {
+
+        public PasswordVault PasswordVault { get; private set; }
+        private MobileServiceClient LoginContinuation = null;
+        private PasswordCredential userAccount;
+
+        public UWPLoginProvider()
+        {
+            PasswordVault = new PasswordVault();
+        }
+
         public async Task LoginAsync(MobileServiceClient client, MobileServiceAuthenticationProvider provider)
         {
+
+            try
+            {
+                userAccount = PasswordVault.FindAllByResource("dancems").FirstOrDefault();
+            } catch (Exception e)
+            {
+                userAccount = null;
+            }
+            
+
+            if (userAccount != null)
+            {
+                var token = PasswordVault.Retrieve("dancems", userAccount.UserName).Password;
+                if (token != null && token.Length > 0 && !IsTokenExpired(token))
+                {
+                    client.CurrentUser = new MobileServiceUser(userAccount.UserName);
+                    client.CurrentUser.MobileServiceAuthenticationToken = token;
+                    return;
+                }
+            }
+
             try
             {
                 var concreteClient = client as MobileServiceClient; //extensionmethod we need is not on interface but on implementation class
-
                 LoginContinuation = concreteClient;
                 await client.LoginAsync(provider, Constants.Constants.UrlScheme);
+                PasswordVault.Add(new PasswordCredential("dancems", client.CurrentUser.UserId, client.CurrentUser.MobileServiceAuthenticationToken));
             }
             catch (Exception e)
             {
@@ -30,7 +65,7 @@ namespace DanceMS.UWP.Services
 
         }
 
-        private MobileServiceClient LoginContinuation = null;
+        
 
         public void ContinueLogin(Uri uri)
         {
@@ -39,6 +74,38 @@ namespace DanceMS.UWP.Services
                 LoginContinuation.ResumeWithURL(uri);
                 LoginContinuation = null;
             }
+        }
+
+        private bool IsTokenExpired(string token)
+        {
+            //Get just the JWT part of the token
+            var jwt = token.Split(new char[] { '.', '/' })[1];
+
+            //Unde the URL encoding
+            jwt = jwt.Replace('-', '+').Replace('_', '/');
+            switch (jwt.Length % 4)
+            {
+                case 0: break;
+                case 2: jwt += "=="; break;
+                case 3: jwt += "="; break;
+                default:
+                    throw new ArgumentException("The token is not a valid Base64 string.");
+            }
+
+            // Convert to a JSON String
+            var bytes = Convert.FromBase64String(jwt);
+            string jsonString = UTF8Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+
+            // Parse as JSON object and get the exp field value,
+            // which is the expiration date as a JavaScript primative date.
+            JObject jsonObj = JObject.Parse(jsonString);
+            var exp = Convert.ToDouble(jsonObj["exp"].ToString());
+
+            // Calculate the expiration by adding the exp value (in seconds) to the
+            // base date of 1/1/1970.
+            DateTime minTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            var expire = minTime.AddSeconds(exp);
+            return (expire < DateTime.UtcNow);
         }
     }
 }
